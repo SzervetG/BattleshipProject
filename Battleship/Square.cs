e7598252df7bb606c2ee30ca27cf5c371ca708cd
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-11-28 11:19:51 +0100

    Add constructor and ChangeStatus method

--- Battleship/Square.cs
+++ Battleship/Square.cs
@@ -2,5 +2,16 @@
 
 public class Square
 {
-    
+    public SquareStatus Status { get; private set; }
+
+    public Square()
+    {
+        Status = SquareStatus.Ocean;
+    }
+
+
+    public void ChangeStatus(SquareStatus status)
+    {
+        this.Status = status;
+    }
 }
