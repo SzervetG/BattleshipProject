ea0fd1e31119bda6246c6feb1721c30fb677730f
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-11-28 11:16:21 +0100

    Add every input asking method based on display class

--- Battleship/Input.cs
+++ Battleship/Input.cs
@@ -2,5 +2,65 @@
 
 public class Input
 {
-    
+    private Validation _validation = new Validation();
+    public string GetName()
+    {
+        string name = Console.ReadLine();
+        while (!_validation.ValidateName(name))
+        {
+            name = Console.ReadLine();
+        }
+        return name;
+    }
+
+
+    public int GetBoardSize()
+    {
+        int boardSize = Convert.ToInt32(Console.ReadLine());
+
+        while (!_validation.ValidateBoardSize(boardSize))
+        {
+            boardSize = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return boardSize;
+    }
+
+
+    public (int, int) GetMove(int boardSize)
+    {
+        string move = Console.ReadLine();
+
+        while (!_validation.ValidateMove(move, boardSize))
+        {
+            move = Console.ReadLine();
+        }
+        (int, int) convertedMove = ConvertMove(move);
+
+        return convertedMove;
+    }
+
+
+    private (int, int) ConvertMove(string move)
+    {
+        (int, int) convertedMove;
+
+        convertedMove.Item1 = move[0];
+        convertedMove.Item2 = Convert.ToInt32(move.Remove(0, 1));
+        
+        return convertedMove;
+    }
+
+
+    public string GetDirection()
+    {
+        string direction = Console.ReadLine();
+
+        while (!_validation.ValidateDirection(direction))
+        {
+            direction = Console.ReadLine();
+        }
+
+        return direction;
+    }
 }
