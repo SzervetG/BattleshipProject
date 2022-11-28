074eb6679d88735026b3318411f5a649efda5fab
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-11-28 12:54:42 +0100

    Update Game class

--- Battleship/Game.cs
+++ Battleship/Game.cs
@@ -2,15 +2,8 @@
 
 public class Game
 {
-    /* Ship placing rule:
-     
-       unit = Convert.ToInt32(Math.Floor(boardsize * boardsize / 2 % 15));
-       5 Long * unit
-       4 Long * unit + 1
-       3 Long * unit + 2
-       2 Long * unit + 3
-       1 Long - Convert.ToInt32(Math.Floor(boardsize * boardsize / 2)) - (4*unit + 6)
-    */
+    
+    public List<Player> Players { get; }
 
 
     public void Gameplay()
