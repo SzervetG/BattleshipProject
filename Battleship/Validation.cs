9aca66548853292c9330fa76786e28969f8749c3
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-12-20 11:35:09 +0100

    Add shipHit/shipSink/GameLoss validations

--- Battleship/Validation.cs
+++ Battleship/Validation.cs
@@ -3,9 +3,9 @@
 public class Validation
 {
     
-    public bool ValidateName(string name)
+    public bool ValidateValueIsNotNothing(string value)
     {
-        if (name.Length < 1)
+        if (value.Length < 1)
         {
             return false;
         }
@@ -14,11 +14,11 @@ public class Validation
     }
 
 
-    public bool ValidateBoardSize(int boardSize)
+    public bool ValidateBoardSize(string boardSize)
     {
-        bool result = boardSize.ToString().Any(char.IsLetter);
+        bool result = boardSize.Any(char.IsLetter);
 
-        if (result || boardSize < 1 || boardSize > 10)
+        if (result || Convert.ToInt32(boardSize) < 5 || Convert.ToInt32(boardSize) > 10)
         {
             return false;
         }
@@ -37,8 +37,8 @@ public class Validation
         string rest = move.Remove(0, 1);
         
 
-        if (!moveLetter.Any(char.IsLetter) || letterIndex >= boardSize ||
-            !TryConvertStringToInt(rest) || Convert.ToInt32(rest) >= boardSize)
+        if (!moveLetter.Any(char.IsLetter) || letterIndex > boardSize ||
+            !TryConvertStringToInt(rest) || Convert.ToInt32(rest) > boardSize)
         {
             return false;
         }
@@ -73,7 +73,7 @@ public class Validation
 
     public bool ValidateDirection(string direction)
     {
-        if (direction.ToLower() != "h" || direction.ToLower() != "v")
+        if (direction.ToLower() != "h" && direction.ToLower() != "v")
         {
             return false;
         }
@@ -92,16 +92,81 @@ public class Validation
             {
                 if (direction.ToLower() == "h")
                 {
-                    if (board.PlayerBoard[firstPartPlace.Item1, firstPartPlace.Item2 + partNum].Status == SquareStatus.Ship ||
-                        firstPartPlace.Item2 + partNum >= boardSize)
+                    if (firstPartPlace.Item2 + partNum >= boardSize || board.PlayerBoard[firstPartPlace.Item1, firstPartPlace.Item2 + partNum].Status == SquareStatus.Ship)
                         return false;
                 }
-                if (board.PlayerBoard[firstPartPlace.Item1 + partNum, firstPartPlace.Item2].Status == SquareStatus.Ship ||
-                    firstPartPlace.Item1 + partNum >= boardSize)
-                    return false;
+                else if (direction.ToLower() == "v")
+                {
+                    if (firstPartPlace.Item1 + partNum >= boardSize || board.PlayerBoard[firstPartPlace.Item1 + partNum, firstPartPlace.Item2].Status == SquareStatus.Ship)
+                        return false;
+                }
             }
         }
 
         return board.PlayerBoard[firstPartPlace.Item1, firstPartPlace.Item2].Status != SquareStatus.Ship;
+    }
+
+
+    public bool ValidateHit(Player playerBeingHit, (int, int)move)
+    {
+        foreach (Ship ship in playerBeingHit.Ships)
+        {
+            foreach ((int, int) coordinate in ship.Coordinates)
+            {
+                if (coordinate.Item1 == move.Item1 && coordinate.Item2 == move.Item2)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    
+    
+    public bool ValidateSink(Player playerBeingHit)
+    {
+        foreach (Ship ship in playerBeingHit.Ships)
+        {
+            int hitPart = 0;
+            foreach ((int, int) coordinate in ship.Coordinates)
+            {
+                if (playerBeingHit.Board.PlayerBoard[coordinate.Item1, coordinate.Item2].Status == SquareStatus.Hit)
+                {
+                    hitPart += 1;
+                }
+            }
+
+            if (hitPart == (int)ship.Type)
+                return true;
+        }
+
+        return false;
+    }
+
+
+    public bool ValidateLost(Player playerBeingHit)
+    {
+        int shipCount = playerBeingHit.Ships.Count;
+        int sunkShip = 0;
+        foreach (Ship ship in playerBeingHit.Ships)
+        {
+            int sunkPart = 0;
+            foreach ((int, int) coordinate in ship.Coordinates)
+            {
+                if (playerBeingHit.Board.PlayerBoard[coordinate.Item1, coordinate.Item2].Status == SquareStatus.Sank)
+                {
+                    sunkPart += 1;
+                }
+            }
+
+            if (sunkPart == (int)ship.Type)
+                sunkShip += 1;
+        }
+
+        if (sunkShip == shipCount)
+            return true;
+        return false;
+
     }
 }
